5601829ee0a7b34201151abd728c0dbbb34d89ff
Thạch Lê <thachle@Thachs-Mac-mini.local>
2022-10-10 10:41:05 +0700

    test

---
+++ JellyTest/JellyTestManager.swift
@@ -0,0 +1,15 @@
+//
+//  JellyTestManager.swift
+//  JellyTest
+//
+//  Created by Thạch Lê on 10/10/2022.
+//
+
+import Foundation
+
+public class JellyTestManager {
+    public func showTest(test: String) {
+        print("test \(test)")
+        
+    }
+}
---
+++ Output/archives/ios_devices.xcarchive/Products/Library/Frameworks/JellyTest.framework/Headers/JellyTest.h
@@ -0,0 +1,18 @@
+//
+//  JellyTest.h
+//  JellyTest
+//
+//  Created by Thạch Lê on 10/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JellyTest.
+FOUNDATION_EXPORT double JellyTestVersionNumber;
+
+//! Project version string for JellyTest
+FOUNDATION_EXPORT const unsigned char JellyTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JellyTest/PublicHeader.h>
+
+
